168ae884432d581dcde1ae0618f7fc5276f3c8b9
irabi007 <kurniawan.skm89@gmail.com>
2025-05-09 04:45:00 +0800

    tes

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['session485'] = array ('1'=>'session485!','session485@|@','session485#|#','session485%','session485^','session485&Rp','session485*','session485(','session485)','session485=30','session485@!@disable','session485#!#');
---
+++ core/page_route.php
@@ -0,0 +1,7 @@
+<?php
+// Dummy page_route.php - tanpa proteksi host/domain
+// Memungkinkan akses via domain seperti mikhmon.test
+function forbPHP()
+{
+    // Fungsi kosong, tidak melakukan validasi apa-apa
+}
